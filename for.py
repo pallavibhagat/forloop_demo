4a2b013682cb7357688e4f9f51f05a7be6d4b521
pallavibhagat <akumari@zeomega.com>
2014-01-10 18:16:12 +0530

    Update for.py

--- for.py
+++ for.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
- "this is for loop prog done by pallavi bhagat"
-=======
 "this is for loop prog done by bhagat"
->>>>>>> c2c08e6640162ab17093925e48cb8b389bc774da
 def fun():
     for i in range(10):
         print i
